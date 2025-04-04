4eaa218bbe23cf68983c0dd04acbd2347156d536
diim <dmitry.besmertny@gmail.com>
2025-04-05 00:06:42 +0300

    tools setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,15 @@
- webpack.mix.jsconst mix = require('laravel-mix');
+const mix = require('laravel-mix');
+const fs = require('fs');
+const path = require('path');
 
+// Get all .scss files from the assets/scss directory
+const scssFiles = fs.readdirSync('assets/scss')
+    .filter(file => file.endsWith('.scss'))
+    .map(file => path.join('assets/scss', file));
 
- mix.sass('assets/scss/app.scss', 'assets/css')
-    .minify('public/css/app.css');
+// Process each file individually
+scssFiles.forEach(file => {
+    const filename = path.basename(file, '.scss');
+    mix.sass(file, 'assets/css')
+       .minify(`assets/css/${filename}.css`);
+});
